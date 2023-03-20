04f05e3dcf0daf4091e2c136026c707741a91c9e
Jineth Cano <mabycano02@gmail.com>
2023-03-20 10:13:25 -0500

    agregar proyecto

---
+++ src/main/java/com/trafic/trafic_web/TraficWebApplication.java
@@ -0,0 +1,13 @@
+package com.trafic.trafic_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraficWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraficWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trafic/trafic_web/TraficWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafic.trafic_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraficWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
